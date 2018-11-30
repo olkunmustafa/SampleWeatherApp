443cd640f3fa3668928afd5f346591abca0ddf65
olkunmustafa <olkun.mustafa@gmail.com>
2018-11-30 14:05:21 +0300

    v0.1 -> The models replaced.

---
+++ app/src/main/java/com/olkunmustafa/sampleweatherapp/model/Clouds.java
@@ -0,0 +1,21 @@
+
+package com.olkunmustafa.sampleweatherapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Clouds {
+
+    @SerializedName("all")
+    @Expose
+    private long all;
+
+    public long getAll() {
+        return all;
+    }
+
+    public void setAll(long all) {
+        this.all = all;
+    }
+
+}
--- app/src/main/java/com/olkunmustafa/sampleweatherapp/model/Weather.kt
+++
@@ -1,4 +0,0 @@
-package com.olkunmustafa.sampleweatherapp.model
-
-class Weather {
-}

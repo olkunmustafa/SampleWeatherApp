7ef424ed6516cd2c29f47cc7384cfb7fbb2421fb
olkunmustafa <olkun.mustafa@gmail.com>
2018-12-06 19:06:59 +0300

    v0.3 -> Weather database converted to persistence one.

--- app/src/main/java/com/olkunmustafa/sampleweatherapp/data/storage/WeatherDatabase.kt
+++ app/src/main/java/com/olkunmustafa/sampleweatherapp/data/storage/WeatherDatabase.kt
@@ -20,7 +20,10 @@ abstract class WeatherDatabase : RoomDatabase() {
         fun getDatabase(context: Context): WeatherDatabase {
 
             if (INSTANCE == null) {
-                INSTANCE = Room.inMemoryDatabaseBuilder(context, WeatherDatabase::class.java)
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    WeatherDatabase::class.java,
+                    "weather-request")
                     .build()
 
             }

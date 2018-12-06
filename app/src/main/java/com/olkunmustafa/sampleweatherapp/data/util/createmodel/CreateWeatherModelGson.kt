c70c7f695d2902ab86d9a2c26f2dede08c4c9451
olkunmustafa <olkun.mustafa@gmail.com>
2018-12-06 16:22:06 +0300

    v0.3 -> Util method added to ICreateWeatherModel.kt to create new WeatherRequest model from Weather model.

--- app/src/main/java/com/olkunmustafa/sampleweatherapp/data/util/createmodel/CreateWeatherModelGson.kt
+++ app/src/main/java/com/olkunmustafa/sampleweatherapp/data/util/createmodel/CreateWeatherModelGson.kt
@@ -1,9 +1,12 @@
 package com.olkunmustafa.sampleweatherapp.data.util.createmodel
 
 import com.google.gson.Gson
+import com.olkunmustafa.sampleweatherapp.data.storage.WeatherRequest
 import com.olkunmustafa.sampleweatherapp.model.Weather
 import io.reactivex.Observable
 import java.lang.NullPointerException
+import java.sql.Date
+import java.util.*
 
 class CreateWeatherModelGson(
     private val gson: Gson?
@@ -19,6 +22,18 @@ class CreateWeatherModelGson(
                 val weather = this.gson?.fromJson(data, Weather::class.java)
                 Observable.just(weather)
             }
+    }
+
+    override fun createWeatherRequestModel(weather: Weather): Observable<WeatherRequest> {
+
+        return Observable.just(WeatherRequest())
+            .map {
+                it.requestTime = Date(Calendar.getInstance().timeInMillis)
+                it.weatherdata = this.gson?.toJson( weather )
+
+                it
+            }
+
     }
 
 }
